0d2c2ed6a12564decdb2355dd687eecb961cc6a7
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-02 14:41:28 -0500

    Created our shared.php file and tested it
    Let's keep our shared components in the shared.php file

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Latest Javascript for bootstrap -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-    <title>The G.U.L.F.</title>
+<?php session_start();
+include("shared.php");
+echo "$HTMLHeader";
+?>
 
 
 </head>
