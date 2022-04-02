0ce1444bce74a268d95d1ad86fb98791ea94bb77
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-02 16:54:24 -0500

    Temporarily allowing the mobile nav bar to close
    The mobile navigation menu has issues with closing and opening. This javascript is only present on the index.php page for now. If we want to use this "bandaid" on other pages, we'll need to include this code on those pages.
    
    Long-term, we need an actual solution to the mobile menu not closing properly. Probably want to discuss with Dr. Jang.

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ echo "$component_HTMLHeader";?>
 <footer>
   <p class="centeredText">Website by Something Visionary - CTEC 4350 @ UT Arlington - For class use only</p>
 </footer>
-
+<script>
+/*This code ensures our nav bar is able to be toggled closed. */
+    $("#navbarSupportedContent a:not(.dropdown-toggle)").click(function() {
+      $("#navbarSupportedContent").collapse("hide");
+    });
+  </script>
 </body>
 </html>
