322b1bfd2f2552b8aaaa47363df486b943ad250b
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-05 20:01:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php session_start();
 include("shared.php");
-echo "$component_HTMLHeader";?>
+echo "$component_HTMLHeader";
+/*
+Link to the wireframes for this project: https://darinhardin550576.invisionapp.com/freehand/Wireframes-YASr5nqZY?projectID=cl09vkvs0jk590132e7033lju
+*/
+?>
 </head>
 
 <body>
