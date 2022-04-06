42ed584c91053686b6a03be6327666973a0b836c
pmyon <44069084+pmyon@users.noreply.github.com>
2022-04-06 17:08:18 -0500

    added to header component
    - added ring central embeddable & fontawesome to header

--- shared.php
+++ shared.php
@@ -11,6 +11,18 @@ $component_HTMLHeader =
 <link rel=\"stylesheet\" href=\"styles.css\">
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>
+<!-- RingCentral embeddable -->
+  <script>
+    (function() {
+      var rcs = document.createElement('script');
+      rcs.src = 'https://ringcentral.github.io/ringcentral-embeddable/adapter.js';
+      var rcs0 = document.getElementsByTagName('script')[0];
+      rcs0.parentNode.insertBefore(rcs, rcs0);
+    })();
+  </script>
+  <!-- FontAwesome icons -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+
 
 <title>The G.U.L.F.</title>
 
