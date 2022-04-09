3a6fb0e6b25d40b995994ee5381e516453af35c3
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-09 14:18:01 -0500

    home page 1 column layout

--- index.php
+++ index.php
@@ -19,99 +19,145 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
 <main>
 <?php echo "$component_Nav";?>
 
-<div class="container-fluid fullwidthBridgePic px-0">
+<div class="container-fluid fullwidthBridgePic px-0 mx-auto">
+    <div id="homepageHeroJumbotron" class="jumbotron row mx-4">
+      <h1 class="display-4 text-white">Giving Us Leadership And Focus</h1>
+      <p class="lead text-white">Delivering fun, positive growth opportunities to DFW's youth since 2016.</p>
 
-  <div id="homepageHeroJumbotron" class="jumbotron row mx-auto">
-    <h1 class="display-4 text-white">Giving Us Leadership And Focus</h1>
-    <p class="lead text-white">Delivering fun, positive growth opportunities to DFW's youth since 2007.</p>
-    <hr class="my-4">
-    <p></p>
-    <p class="lead text-white">
+      <p></p>
+      <p class="lead text-white">
       Want to get involved?
-    </p>
-    <p class="lead">
+      </p>
+      <p class="lead">
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
-    </p>
-  </div>
-  <div id="getInvolvedJumbotron" class="jumbotron row mx-auto">
-    <div class="card col-xl-3 mx-2">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Join The G.U.L.F.</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Learn More</a>
-      </div>
+      </p>
+    </div>
+    <div id="getInvolvedJumbotron" class="jumbotron row mx-4">
+
+
+      <!-- center-aligned section -->
+      <section>
+      <h1 class="display-4 text-white text-center">Volunteering</h1>
+      <p class="lead text-white text-center">Help us accomplish our mission to develop the next generation of leaders and entrepreneurs.</p>
+
+      <p></p>
+      <p class="lead text-white text-center">
+      Become a volunteer today.
+      </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Learn More</a>
+      </p>
+      </section>
+
+      <!-- center-aligned section -->
+      <section>
+      <h1 class="display-4 text-white text-center">Membership</h1>
+      <p class="lead text-white text-center">Join The G.U.L.F. and experience the growth opportunities of a life time.</p>
+
+      <p></p>
+      <p class="lead text-white text-center">
+      Ready to join?
+      </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Learn More</a>
+      </p>
+      </section>
+
+      <!-- center-aligned section -->
+      <section>
+      <h1 class="display-4 text-white text-center">Sponsorship</h1>
+      <p class="lead text-white text-center">Join The G.U.L.F as a sponsor and help us deliver world-class growth opportunities for DFW youth.</p>
+
+      <p></p>
+      <p class="lead text-white text-center">
+      Interested in becoming a sponsor?
+      </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Learn More</a>
+      </p>
+      </section>
+
+
+    <!-- Not using these cards rn, might delete them -->
+    <!-- <div class="card col-xl-3 mx-2">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Join The G.U.L.F.</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
     </div>
     <div class="card col-xl-3 mx-2">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Volunteer With Us</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Learn More</a>
-      </div>
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Volunteer With Us</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
     </div>
     <div class="card col-xl-3 mx-2">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Invest In Our Youth</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Learn More</a>
-      </div>
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Invest In Our Youth</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Learn More</a>
     </div>
-  </div>
-  <div id="testimonialsJumbotron" class="jumbotron row mx-auto">
-  </div>
-  <div id="statisticsJumbotron row">
+    </div>
+    -->
+    </div>
+    <div id="testimonialsJumbotron" class="jumbotron row mx-4">
+    </div>
+    <div id="statisticsJumbotron" class="row mx-4">
     <!-- counter attempt with code from codepen -->
     <section id="counter" class="counter">
-            <div class="col">
-                <div class="overlay p-y-3">
-                    <div class="container">
-                        <div class="row">
-                            <div class="main_counter_content flexStatisticBoxes text-center white-text wow fadeInUp mx-auto">
-                                <div class="col-md-3">
-                                    <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                        <i class="fa fa-heart m-b-1 gulfBlueBG text-white"></i>
-                                        <h2 class="statistic-counter gulfBlueBG text-white">100</h2>
-                                        <p class="gulfBlueBG text-white">Side Hustles Started</p>
-                                    </div>
+        <div class="col">
+            <div class="overlay p-y-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="main_counter_content flexStatisticBoxes text-center white-text wow fadeInUp mx-auto">
+                            <div class="col-md-3">
+                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                    <i class="fa fa-heart m-b-1 gulfBlueBG text-white"></i>
+                                    <h2 class="statistic-counter gulfBlueBG text-white">100</h2>
+                                    <p class="gulfBlueBG text-white">Side Hustles Started</p>
                                 </div>
-                                <div class="col-md-3">
-                                    <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                        <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
-                                        <h2 class="statistic-counter gulfBlueBG text-white">400</h2>
-                                        <p class="gulfBlueBG text-white">Volunteering Events</p>
-                                    </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                    <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
+                                    <h2 class="statistic-counter gulfBlueBG text-white">400</h2>
+                                    <p class="gulfBlueBG text-white">Volunteering Events</p>
                                 </div>
-                                <div class="col-md-3">
-                                    <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                        <i class="fa fa-refresh m-b-1 gulfBlueBG text-white"></i>
-                                        <h2 class="statistic-counter gulfBlueBG text-white">312</h2>
-                                        <p class="gulfBlueBG text-white">Internships Created</p>
-                                    </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                    <i class="fa fa-refresh m-b-1 gulfBlueBG text-white"></i>
+                                    <h2 class="statistic-counter gulfBlueBG text-white">312</h2>
+                                    <p class="gulfBlueBG text-white">Internships Created</p>
                                 </div>
-                                <div class="col-md-3">
-                                    <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                        <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
-                                        <h2 class="statistic-counter gulfBlueBG text-white">8</h2>
-                                        <p class="gulfBlueBG text-white">Schools Involved</p>
-                                    </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                    <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
+                                    <h2 class="statistic-counter gulfBlueBG text-white">8</h2>
+                                    <p class="gulfBlueBG text-white">Schools Involved</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section><!-- End of counter Section -->
-  </div>
-  <div id="homepageGoogleMapsEmbed">
+        </div>
+    </section><!-- End of counter Section -->
+    </div>
+    <div id="homepageGoogleMapsEmbed">
     <?php echo "$component_GoogleMapsEmbed"?>
-  </div>
+    </div>
 
 
+  <!-- end innerContainer-->
 </div>
-
-<!--end container -->
+<!--end container-fluid -->
 </div>
 </main>
 <?php echo "$component_Footer";?>
