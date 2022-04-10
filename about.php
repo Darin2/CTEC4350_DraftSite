4d5a8a3ad09cc55da046a01846b981974159f850
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-10 10:53:44 -0500

    about page looks decent now

--- about.php
+++ about.php
@@ -7,21 +7,85 @@ echo "$component_HTMLHeader";?>
 
 <main>
 <?php echo "$component_Nav";?>
-  <!-- Thanks for getting started here. Please remember to use Bootstrap if you can. -->
-  <h3>Our Mission</h3>
-  <div class="mission">
+<div id="aboutJumbotron" class="jumbotron row ml-0">
 
+  <!-- Our Mission row -->
+  <div id="about-OurMissionRow" class="row d-flex align-items-center bg-dark bg-gradient">
+    <!-- Our Mission animation. Should show up on left side of text for tablet and desktop. Should show up on top of the text for mobile.  -->
+    <section class="col-sm">
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player
+      src="https://assets3.lottiefiles.com/packages/lf20_xiussssy.json"
+      background="transparent"
+      speed="1"
+      style="width: 100%; height: 80%;"
+      loop
+      autoplay>
+      </lottie-player>
+    </section>
+    <!-- center-aligned text section with CTA button-->
+    <section class="col-sm">
+      <h1 class="display-4 text-white text-center">Our Mission</h1>
+      <p class="lead text-white text-center">We're here to develop the next generation of leaders and entrepreneurs.</p>
+      <!-- commenting this part out for right now. Not sure if we want a CTA in every single section. Might get repetitive.
+      <p class="lead text-white text-center">
+      Become a volunteer today.
+      </p>
+      <p class="lead w-25 d-flex justify-content-center mx-auto">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Learn More</a>
+      </p>
+      -->
+    </section>
   </div>
 
-  <h3>Our Impact</h3>
-  <div class="impact">
+  <!-- Our Impact row -->
+  <div id="about-OurImpactRow"class="row d-flex align-items-center gulfBlueBG">
+    <!-- center-aligned section -->
+    <section class="col-sm">
+      <h1 class="display-4 text-white text-center">Our Impact</h1>
+      <p class="lead text-white text-center">We've led over 100 volunteering events impacting over 20,000 people in 5 cities.</p>
+      <p class="lead text-white text-center">
+      Want to be a part of the action?
+      </p>
+      <p class="lead w-25 d-flex justify-content-center mx-auto">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Get Involved</a>
+      </p>
+    </section>
+    <section class="col-sm">
+      <!-- Our Impact lottie animation. Should show up on right side of text for tablet and desktop. Should show up on top of the text for mobile.  -->
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_dews3j6m.json"
+        background="transparent"
+        speed="1"
+        style="width: 100%; height: 80%;"
+        loop
+        autoplay>
+      </lottie-player>
 
+    </section>
   </div>
 
-  <h3>Our Story</h3>
-  <div class="story">
-
+  <!--Our Story row -->
+  <div id="about-OurStoryRow" class="row d-flex align-items-center">
+    <!-- Our Story lottie animation. should show up on left side of text for tablet/desktop. Should show up above text for mobile. -->
+    <section class="col-sm">
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_qct0ydor.json"  background="#1A5EBB"  speed="1"  style="width: 100%; height: 80%;"  loop  autoplay></lottie-player>
+    </section>
+    <!-- center-aligned text section with CTA button -->
+    <section class="col-sm">
+      <h1 class="display-4 text-black text-center">Our Story</h1>
+      <p class="lead text-black text-center">Since 2016, we've been driving positive change in DFW's youth.</p>
+      <p class="lead text-black text-center">
+      Interested in getting involved?
+      </p>
+      <p class="lead w-25 d-flex justify-content-center mx-auto">
+        <a class="btn btn-primary btn-lg d-flex justify-content-center" href="#" role="button">Learn More</a>
+      </p>
+    </section>
   </div>
+
+</div>
 
 </main>
 
