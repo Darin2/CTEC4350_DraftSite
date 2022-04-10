afe3f183d3dd885ed747f6c0622d1d7072013ed3
brandinole <70599440+brandinole@users.noreply.github.com>
2022-04-10 13:03:58 -0500

    additional format fixes

--- contact.php
+++ contact.php
@@ -9,16 +9,16 @@ echo "$component_HTMLHeader";?>
 <?php echo "$component_Nav";?>
 
 <form class="" action="index.html" method="post">
-  <label for="fname">First Name</label>
+  <label for="fname">First Name</label> <br>
   <input type="text" id="fname" name="fname"><br>
 
-  <label for="lname">Last name</label>
+  <label for="lname">Last name</label> <br>
   <input type="text" id="lname" name="lname"><br>
 
-  <label for="email">Email</label>
+  <label for="email">Email</label> <br>
   <input type="text" id="email" name="email"><br>
 
-  <label for="pnumber">Phone Number</label>
+  <label for="pnumber">Phone Number</label> <br>
   <input type="text" id="pnumber" name="pnumber"><br>
 
   <label for="interest">I am interested in...</label><br>
