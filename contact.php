e94bb09605349b1fbe43c336cbca7689f330a133
brandinole <70599440+brandinole@users.noreply.github.com>
2022-04-10 13:02:07 -0500

    added basic contact form
    needs styling (bootstrap)
    fixed format

--- contact.php
+++ contact.php
@@ -12,14 +12,14 @@ echo "$component_HTMLHeader";?>
   <label for="fname">First Name</label>
   <input type="text" id="fname" name="fname"><br>
 
-  <label for="lname">Last name</label><br>
-  <input type="text" id="lname" name="lname">
+  <label for="lname">Last name</label>
+  <input type="text" id="lname" name="lname"><br>
 
-  <label for="email">Email</label><br>
-  <input type="text" id="email" name="email">
+  <label for="email">Email</label>
+  <input type="text" id="email" name="email"><br>
 
-  <label for="pnumber">Phone Number</label><br>
-  <input type="text" id="pnumber" name="pnumber">
+  <label for="pnumber">Phone Number</label>
+  <input type="text" id="pnumber" name="pnumber"><br>
 
   <label for="interest">I am interested in...</label><br>
   <select id="interested" name="interested">
