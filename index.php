14d27029d722a2be72ba678c844ef4510430e147
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-09 21:37:17 -0500

    editing stats section a bit

--- index.php
+++ index.php
@@ -20,8 +20,9 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
 <?php echo "$component_Nav";?>
 
 <div class="container-fluid fullwidthBridgePic px-0 mx-auto">
+
     <div id="homepageHeroJumbotron" class="jumbotron row mx-4">
-      <h1 class="display-4 text-white">Giving Us Leadership And Focus</h1>
+      <h1 class="display-4 text-white pt-5">Giving Us Leadership And Focus</h1>
       <p class="lead text-white my-0 py-0">Delivering fun, positive growth opportunities to DFW's youth since 2016.</p>
       <p class="lead">
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
@@ -47,8 +48,6 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <section class="col-sm">
           <h1 class="display-4 text-white text-center">Volunteering</h1>
           <p class="lead text-white text-center">Help us accomplish our mission to develop the next generation of leaders and entrepreneurs.</p>
-
-          <p class="lead text-white text-center">***place carousel or bootstrap card(s) with volunteering-related image(s) and text here ****</p>
           <p class="lead text-white text-center">
           Become a volunteer today.
           </p>
@@ -64,8 +63,6 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <section class="col-sm">
         <h1 class="display-4 text-white text-center">Membership</h1>
         <p class="lead text-white text-center">Join The G.U.L.F. and experience the growth opportunities of a life time.</p>
-
-        <p class="lead text-white text-center">***place carousel or bootstrap card(s) with membership-related image(s) and text here ****</p>
         <p class="lead text-white text-center">
         Ready to join?
         </p>
@@ -105,7 +102,6 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <section class="col-sm">
           <h1 class="display-4 text-black text-center">Sponsorship</h1>
           <p class="lead text-black text-center">Join The G.U.L.F as a sponsor and help us deliver world-class growth opportunities for DFW youth.</p>
-          <p class="lead text-black text-center">***place carousel or bootstrap card(s) with sponsorship-related image(s) and text here ****</p>
           <p class="lead text-black text-center">
           Interested in becoming a sponsor?
           </p>
@@ -118,54 +114,52 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
     </div>
     <div id="testimonialsJumbotron" class="jumbotron row mx-4">
     </div>
-    <div id="statisticsJumbotron" class="row mx-4">
-    <!-- counter attempt with code from codepen -->
-    <section id="counter" class="counter">
-        <div class="col">
-            <div class="overlay p-y-3">
-                <div class="container">
-                    <div class="row">
-                        <div class="main_counter_content flexStatisticBoxes text-center white-text wow fadeInUp mx-auto">
-                            <div class="col-md-3">
-                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                    <i class="fa fa-heart m-b-1 gulfBlueBG text-white"></i>
-                                    <h2 class="statistic-counter gulfBlueBG text-white">100</h2>
-                                    <p class="gulfBlueBG text-white">Side Hustles Started</p>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                    <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
-                                    <h2 class="statistic-counter gulfBlueBG text-white">400</h2>
-                                    <p class="gulfBlueBG text-white">Volunteering Events</p>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                    <i class="fa fa-refresh m-b-1 gulfBlueBG text-white"></i>
-                                    <h2 class="statistic-counter gulfBlueBG text-white">312</h2>
-                                    <p class="gulfBlueBG text-white">Internships Created</p>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
-                                    <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
-                                    <h2 class="statistic-counter gulfBlueBG text-white">8</h2>
-                                    <p class="gulfBlueBG text-white">Schools Involved</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section><!-- End of counter Section -->
+    <div id="statisticsJumbotron" class="row gulfBlueBG">
+      <!-- counter attempt with code from codepen -->
+      <section id="counter" class="counter">
+          <div class="col">
+              <div class="overlay p-y-3">
+                  <div class="container">
+                      <div class="row">
+                          <div class="main_counter_content flexStatisticBoxes text-center white-text wow fadeInUp mx-auto">
+                              <div class="col-md-3">
+                                  <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                      <i class="fa fa-heart m-b-1 gulfBlueBG text-white"></i>
+                                      <h2 class="statistic-counter gulfBlueBG text-white">100</h2>
+                                      <p class="gulfBlueBG text-white">Side Hustles Started</p>
+                                  </div>
+                              </div>
+                              <div class="col-md-3">
+                                  <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                      <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
+                                      <h2 class="statistic-counter gulfBlueBG text-white">400</h2>
+                                      <p class="gulfBlueBG text-white">Volunteering Events</p>
+                                  </div>
+                              </div>
+                              <div class="col-md-3">
+                                  <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                      <i class="fa fa-refresh m-b-1 gulfBlueBG text-white"></i>
+                                      <h2 class="statistic-counter gulfBlueBG text-white">312</h2>
+                                      <p class="gulfBlueBG text-white">Internships Created</p>
+                                  </div>
+                              </div>
+                              <div class="col-md-3">
+                                  <div class="single_counter p-y-2 m-t-1 mx-4 gulfBlueBG">
+                                      <i class="fa fa-check m-b-1 gulfBlueBG text-white"></i>
+                                      <h2 class="statistic-counter gulfBlueBG text-white">8</h2>
+                                      <p class="gulfBlueBG text-white">Schools Involved</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section><!-- End of counter Section -->
     </div>
     <div id="homepageGoogleMapsEmbed">
     <?php echo "$component_GoogleMapsEmbed"?>
     </div>
-
-
   <!-- end innerContainer-->
 </div>
 <!--end container-fluid -->
