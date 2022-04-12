fd93d93ccfb4cc6d4f40c4e2748b1d1ccb5be480
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-12 08:49:27 -0500

    editing login page code
    test username is thegulf2022
    test password is thegulf2022

--- admin_loginpage.php
+++ admin_loginpage.php
@@ -14,11 +14,11 @@ The user input being validated in this script comes from the HTML form below.
 if (array_key_exists('Submit1', $_POST)){
 
   //If the user gives admin credentials, give admin access
-  if (($_POST['username_string'] == "admin") && ($_POST['password_string']=="123")){
+  if (($_POST['username_string'] == "thegulf2022") && ($_POST['password_string']=="thegulf2022")){
     //start a session
     $_SESSION = array();
     //create a name that we'll use to address the user (e.g. "Welcome back, Administrator.")
-    $_SESSION['welcomepageusername']="Administrator";
+    $_SESSION['welcomepageusername']="Mr Bible";
 
     //***Debugging code below:
     //echo "session welcome page username thing = ".$_SESSION['welcomepageusername'];
@@ -41,63 +41,75 @@ if (array_key_exists('Submit1', $_POST)){
   */
   //else {
       //If the user doesn't enter admin credentials, store the username and password in their own variables
-      $login_attempt_username = $_POST['username_string'];
-      //$login_attempt_password = $_POST['password_string'];
-      $login_attempt_password = hash('sha256', $_POST['password_string']); //this line encrypts the password string using sha256. Must implement the same encryption on the password entered at registration (registration_page.php) for this to work.
+        //$login_attempt_username = $_POST['username_string'];
+        //$login_attempt_password = hash('sha256', $_POST['password_string']); //this line encrypts the password string using sha256. Must implement the same encryption on the password entered at registration (registration_page.php) for this to work.
 
       /*Building a sql query that will use bindparam() to deter SQL injection attacks
-      This query will go to the credentials table (deeplyUserTable) and look for a record that has matching username and password.
-      If the database finds a record for this query, we want to give access to the user, and grab the username, password and email for that record (email will be used for signup confirmations, password recovery in the future)
-      */
-      $sql = "SELECT firstname, username, password, email FROM `deeplyUserTable` WHERE username = ? AND password = ?";
 
-      $stmt = $conn->stmt_init();
+        This query will go to the credentials table (GULFUserTable) and look for a record that has matching username and password.
+
+        If the database finds a record for this query, we want to give access to the user, and grab the username, password and email for that record (email will be used for signup confirmations, password recovery in the future)
+      */
+
+      //$sql = "SELECT firstname, username, password, email FROM `GULFUserTable` WHERE username = ? AND password = ?";
+
+      //$stmt = $conn->stmt_init();
 
       //Sending query to database and storing results
-      if($stmt->prepare($sql)){
+      //if($stmt->prepare($sql)){
         //echo "Debugging: the statement prepared<br>";
-        $stmt->bind_param('ss', $login_attempt_username, $login_attempt_password);
+        //$stmt->bind_param('ss', $login_attempt_username, $login_attempt_password);
 
-        $stmt->execute();
+        //$stmt->execute();
 
-        $stmt->bind_result($firstname,$username,$password,$email);
+        //$stmt->bind_result($firstname,$username,$password,$email);
 
-        $stmt->store_result();
+        //$stmt->store_result();
 
-        if($stmt->num_rows == 1){
-          //echo "Debugging: there was one record<br>";
-          $stmt->fetch();
+        //if($stmt->num_rows == 1){
+          //echo "Debugging:  $stmt->num_rows == 1. there was one record<br>";
+          //$stmt->fetch();
           //I think I can just give the user access right here because the statement wouldn't return a result if the credentials didn't match
 
-          //Giving access to the user via session variable
-          $_SESSION = array();
-          $_SESSION['firstNameOfUser'] = $firstname;
-          $_SESSION['welcomepageusername']=$username;
-          $_SESSION['usernameForNav']=$username;
-          $usernameForNav = $_SESSION['usernameForNav'];
+          //***Giving access to the user via session variable
+          //Start a session
+          //$_SESSION = array();
+          //Take the $firstname string from our query and store it in a session variable
+          //$_SESSION['firstNameOfUser'] = $firstname;
+          //Take the $username string from our query and store it in a session variable (this string is printed in the Welcome Page greeting)
+          //$_SESSION['welcomepageusername']=$username;
+          //Take the $username string from our query and store it in a session variable (this string is printed in the Nav bar as "Hello, $username")
+          //$_SESSION['usernameForNav']=$username;
+          //create a non-session variable for the username
+          //$usernameForNav = $_SESSION['usernameForNav'];
+
+          /*Debugging stuff*/
           //echo "Checking what's inside the session welcome page username thing = ".$_SESSION['welcomepageusername'];
           //echo "Debugging: username = $username";
-          $_SESSION['user_access'] = true;
 
+          //Give user level access
+          //$_SESSION['user_access'] = true;
 
+          /*Debugging stuff*/
           //echo "The SQL query returned a result, so the password was correct and you're now logged in";
 
-          $stmt->close();
+          //$stmt->close();
 
-          //Redirecting to welcome page
-          header("Location: welcomepage.php");
-          exit;
+          //Our user is logged in so we redirect them to the welcome page
+          //Note that we can't echo ANYTHING to the page prior to this line.
+          //header("Location: welcomepage.php");
+          //exit;
 
-        }
+        //}
         //stmt->num_rows was not ==1
-        else{
-          echo "Incorrect username or password. To reset your password, please click <a href='passwordreset.php'>here</a>.";
-        }
+        //else{
+          //echo "Incorrect username or password. To reset your password, please click <a href='passwordreset.php'>here</a>.";
+      //  }
             /*
             Found this at https://www.sitepoint.com/community/t/efficient-way-to-check-if-a-mysql-query-returned-no-results/10514
             Hopefully this will allow me to check whether the statement returned anything */
-      }
- }
+      //}
+// }
 }
 //Commented out: this is a Login mechanism that uses plain text passwords stored in the file. It does not interface with a database.
 /*
@@ -143,9 +155,7 @@ if (array_key_exists('Submit1', $_POST)){
 
 <!-- Headers-->
 <?php
-    //include("shared.php");
     echo "$component_HTMLHeader";
-
 ?>
 <!-- Printing Login Form -->
 <body>
