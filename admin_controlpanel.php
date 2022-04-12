f96e38b6a8974aab76f644c1282e4e1f8267156b
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-12 14:51:25 -0500

    adding basic text content to admin control panel page

--- admin_controlpanel.php
+++ admin_controlpanel.php
@@ -12,6 +12,9 @@ $conn = dbConnect();
 <main>
   <body>
     <h1>Welcome to the admin control panel, <?php echo $_SESSION['admin_Name'];?>!</h1>
+    <div class="container bg-light bg-gradient">
+      <p>Our admin control panel stuff will go here </p>
+    </div>
   </body>
 </main>
 <?php echo $footer ?>
