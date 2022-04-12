38344da78218115b71040145769bcd77829ab652
vanessatorq <vmt8625@mavs.uta.edu>
2022-04-12 12:20:03 -0500

    Update challenge.php

--- challenge.php
+++ challenge.php
@@ -1,6 +1,5 @@
 <?php session_start();
-include("shared.php");
-include("dbconn.inc.php");
+include("../../../Downloads/Current Files/shared.php");
 echo "$component_HTMLHeader";?>
 </head>
 
@@ -8,7 +7,8 @@ echo "$component_HTMLHeader";?>
 
 <main>
 <?php echo "$component_Nav";?>
-
+      <h1 class="display-4 text-white pt-5">Monthly Entrepreneur Challenge</h1>
+	<p class="lead text-white my-0 py-0">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </main>
 
 <?php echo "$component_Footer";?>
