dd1d9f5bf87d12c26f56645120db852eaaab4f5b
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-12 08:37:56 -0500

    explaining stuff with comments

--- admin_loginpage.php
+++ admin_loginpage.php
@@ -3,8 +3,6 @@ session_start();
 include("shared.php");
 include("dbconn.inc.php");
 $conn = dbConnect();
-echo "$component_HTMLHeader";
-
 /*This section attempts to use the database and verify user's credentials when they attempt to log in, and grant access (the only type of access we need to worry about is "admin" level access). We only grant admin access if the credentials are correct.
 
 The user input being validated in this script comes from the HTML form below.
@@ -17,82 +15,93 @@ if (array_key_exists('Submit1', $_POST)){
 
   //If the user gives admin credentials, give admin access
   if (($_POST['username_string'] == "admin") && ($_POST['password_string']=="123")){
+    //start a session
     $_SESSION = array();
+    //create a name that we'll use to address the user (e.g. "Welcome back, Administrator.")
     $_SESSION['welcomepageusername']="Administrator";
+
+    //***Debugging code below:
     //echo "session welcome page username thing = ".$_SESSION['welcomepageusername'];
     //echo "username = $username";
+
+    //Set admin access to true
     $_SESSION['admin_access'] = true;
+    //Redirect the user to the admin control panel. note: you can't echo ANYTHING to the page until we've reached this line, or the whole script will break. That's why our HTML header code is included much further down in the file.
     header("Location: admin_controlpanel.php");
     exit;
   }
-  else {
-  //If the user doesn't enter admin credentials, store the username and password in their own variables
-  $login_attempt_username = $_POST['username_string'];
-  //$login_attempt_password = $_POST['password_string'];
-  $login_attempt_password = hash('sha256', $_POST['password_string']); //this line encrypts the password string using sha256. Must implement the same encryption on the registration page for this to work.
-  /*Building a sql query that will use bindparam() to deter SQL injection attacks
-  This query will go to the credentials table (deeplyUserTable) and look for a record that has matching username and password.
-  If the database finds a record for this query, we want to give access to the user, and grab the username, password and email for that record (email will be used for signup confirmations, password recovery in the future)
+  /*Commented out: this code checks the database for a matching set of credentials (username and password).
+
+  If a matching set of credentials are found, the user is logged in and redirected to the "welcome page" (which does not currently exist in this directory as of 4-12-2022).
+
+  This code is currently unnecessary for our Spring 2022 project but I'm leaving it here for now, on the off chance we need to create a registration/login system.
+
+
+   - Darin
   */
-  $sql = "SELECT firstname, username, password, email FROM `deeplyUserTable` WHERE username = ? AND password = ?";
+  //else {
+      //If the user doesn't enter admin credentials, store the username and password in their own variables
+      $login_attempt_username = $_POST['username_string'];
+      //$login_attempt_password = $_POST['password_string'];
+      $login_attempt_password = hash('sha256', $_POST['password_string']); //this line encrypts the password string using sha256. Must implement the same encryption on the password entered at registration (registration_page.php) for this to work.
 
-  $stmt = $conn->stmt_init();
+      /*Building a sql query that will use bindparam() to deter SQL injection attacks
+      This query will go to the credentials table (deeplyUserTable) and look for a record that has matching username and password.
+      If the database finds a record for this query, we want to give access to the user, and grab the username, password and email for that record (email will be used for signup confirmations, password recovery in the future)
+      */
+      $sql = "SELECT firstname, username, password, email FROM `deeplyUserTable` WHERE username = ? AND password = ?";
 
-  //Sending query to database and storing results
-  if($stmt->prepare($sql)){
-    //echo "Debugging: the statement prepared<br>";
-    $stmt->bind_param('ss', $login_attempt_username, $login_attempt_password);
+      $stmt = $conn->stmt_init();
 
-    $stmt->execute();
+      //Sending query to database and storing results
+      if($stmt->prepare($sql)){
+        //echo "Debugging: the statement prepared<br>";
+        $stmt->bind_param('ss', $login_attempt_username, $login_attempt_password);
 
-    $stmt->bind_result($firstname,$username,$password,$email);
+        $stmt->execute();
 
-    $stmt->store_result();
+        $stmt->bind_result($firstname,$username,$password,$email);
 
-    if($stmt->num_rows == 1){
-      //echo "Debugging: there was one record<br>";
-      $stmt->fetch();
-      //I think I can just give the user access right here because the statement wouldn't return a result if the credentials didn't match
+        $stmt->store_result();
 
-      //Giving access to the user via session variable
-      $_SESSION = array();
-      $_SESSION['firstNameOfUser'] = $firstname;
-      $_SESSION['welcomepageusername']=$username;
-      $_SESSION['usernameForNav']=$username;
-      $usernameForNav = $_SESSION['usernameForNav'];
-      //echo "Checking what's inside the session welcome page username thing = ".$_SESSION['welcomepageusername'];
-      //echo "Debugging: username = $username";
-      $_SESSION['user_access'] = true;
+        if($stmt->num_rows == 1){
+          //echo "Debugging: there was one record<br>";
+          $stmt->fetch();
+          //I think I can just give the user access right here because the statement wouldn't return a result if the credentials didn't match
+
+          //Giving access to the user via session variable
+          $_SESSION = array();
+          $_SESSION['firstNameOfUser'] = $firstname;
+          $_SESSION['welcomepageusername']=$username;
+          $_SESSION['usernameForNav']=$username;
+          $usernameForNav = $_SESSION['usernameForNav'];
+          //echo "Checking what's inside the session welcome page username thing = ".$_SESSION['welcomepageusername'];
+          //echo "Debugging: username = $username";
+          $_SESSION['user_access'] = true;
 
 
-      //echo "The SQL query returned a result, so the password was correct and you're now logged in";
+          //echo "The SQL query returned a result, so the password was correct and you're now logged in";
 
-      $stmt->close();
+          $stmt->close();
 
-      //Redirecting to welcome page
-      header("Location: welcomepage.php");
-      exit;
+          //Redirecting to welcome page
+          header("Location: welcomepage.php");
+          exit;
 
-    }
-    //stmt->num_rows was not ==1
-    else{
-      echo "Incorrect username or password. To reset your password, please click <a href='passwordreset.php'>here</a>.";
-    }
-        /*
-        Found this at https://www.sitepoint.com/community/t/efficient-way-to-check-if-a-mysql-query-returned-no-results/10514
-        Hopefully this will allow me to check whether the statement returned anything */
-  }
+        }
+        //stmt->num_rows was not ==1
+        else{
+          echo "Incorrect username or password. To reset your password, please click <a href='passwordreset.php'>here</a>.";
+        }
+            /*
+            Found this at https://www.sitepoint.com/community/t/efficient-way-to-check-if-a-mysql-query-returned-no-results/10514
+            Hopefully this will allow me to check whether the statement returned anything */
+      }
  }
 }
-//Commented out: this is the first Login mechanism that uses plain text passwords stored in the file (less secure and does not interface with registration system)
+//Commented out: this is a Login mechanism that uses plain text passwords stored in the file. It does not interface with a database.
 /*
   if (array_key_exists('Submit1', $_POST)) { // each page needs a different name for the submit button
-  /*********************/
-  /*
-  - Does not interface with registration system database table
-  - Leaving as an easy way to log in and out
-  -Other login system is lower down in this file
-   */
     //Clearing any session variables
     /*$_SESSION = array();
     $adminPassword = 123;
@@ -135,8 +144,8 @@ if (array_key_exists('Submit1', $_POST)){
 <!-- Headers-->
 <?php
     //include("shared.php");
-    echo "$HTMLHeader";
-    echo "$header";
+    echo "$component_HTMLHeader";
+
 ?>
 <!-- Printing Login Form -->
 <body>
