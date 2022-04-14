2ad4971361462eb4afbddeaa11257c9cb1b989ca
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-14 11:01:59 -0500

    Update admin_controlpanel.php

--- admin_controlpanel.php
+++ admin_controlpanel.php
@@ -15,7 +15,7 @@ header("Location: admin_loginpage.php");
 <main class="bg-dark">
   <body class="bg-dark">
     <!-- control panel container -->
-    <div class="container bg-light bg-gradient mx-auto col-lg-4 col-md-6 col-sm-12 rounded">
+    <div id="controlPanelContainer" class="container bg-light bg-gradient mx-auto col-lg-4 col-md-6 col-sm-12 pb-5 rounded">
 
           <!-- admin links will go in this div -->
           <div class="mx-auto col-lg-6 col-md-8 col-sm-12 my-3 px-0 pt-5 bg-light bg-gradient rounded">
@@ -23,14 +23,21 @@ header("Location: admin_loginpage.php");
             <h1 class="text-center display-4 py-1">Admin Controls</h1>
 
             <!-- card attempt -->
-            <div class="card" style="width: 18rem;">
+            <div class="card mb-4 pb-4" style="width: 18rem;">
               <div class="card-header bg-primary text-white text-center">
-                My Leads
+                My Messages
               </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item"><a href="admin_contactFormLeads.php" class="list-group-item list-group-item-action">View Contact Form submissions</a></li>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item"><a href="admin_contactFormLeads.php" class="list-group-item list-group-item-action">View Messages</a></li>
+              </ul>
+            </div>
+
+            <div class="card mb-4 pb-4" style="width: 18rem;">
+              <div class="card-header bg-primary text-white text-center">
+                Volunteers
+              </div>
+              <ul class="list-group list-group-flush mb-0">
                 <li class="list-group-item"><a href="admin_volunteerInterestFormLeads.php" class="list-group-item list-group-item-action">View Volunteer Interest Form submissions</a></li>
-                <li class="list-group-item"><a href="admin_registeredUsersList.php" class="list-group-item list-group-item-action">View Registered Users</a></li>
               </ul>
             </div>
 
