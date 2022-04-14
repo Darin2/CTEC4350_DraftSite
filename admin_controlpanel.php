de0e8ec216f1b2ffe108f5d9b7ab8961284178c2
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-14 16:57:27 -0500

    experimenting with control panel

--- admin_controlpanel.php
+++ admin_controlpanel.php
@@ -15,14 +15,13 @@ header("Location: admin_loginpage.php");
 <main class="bg-dark">
   <body class="bg-dark">
     <!-- control panel container -->
-    <div id="controlPanelContainer" class="container bg-light bg-gradient mx-auto col-lg-4 col-md-6 col-sm-12 pb-5 rounded">
+    <div id="controlPanelContainer" class="container bg-light bg-gradient mx-auto col-lg-10 col-md-12 col-sm-12 pb-5 rounded">
 
-          <!-- admin links will go in this div -->
+          <!-- admin links will go in this div . card are vertical, not in rows.-->
           <div class="mx-auto col-lg-6 col-md-8 col-sm-12 my-3 px-0 pt-5 bg-light bg-gradient rounded">
 
             <h1 class="text-center display-4 py-1">Admin Controls</h1>
 
-            <!-- card attempt -->
             <div class="card mb-4 pb-4">
               <div class="card-header bg-primary text-white text-center">
                 My Messages
@@ -59,6 +58,52 @@ header("Location: admin_loginpage.php");
               </ul>
             </div>
 
+          </div>
+
+          <!-- trying a new layout for admin links -->
+          <div class="mx-auto col-lg-8 col-md-12 col-sm-12 my-3 px-0 pt-5 bg-light bg-gradient rounded">
+
+            <h1 class="text-center display-4 py-1">Admin Controls</h1>
+
+          <div class="row">
+            <div class="col card mb-4 pb-4">
+              <div class="card-header bg-primary text-white text-center">
+                My Messages
+              </div>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item"><a href="admin_contactFormLeads.php" class="list-group-item list-group-item-action">View Messages</a></li>
+              </ul>
+            </div>
+
+            <div class="col card mb-4 pb-4">
+              <div class="card-header bg-primary text-white text-center">
+                Volunteers
+              </div>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item"><a href="admin_volunteerInterestFormLeads.php" class="list-group-item list-group-item-action">View View Volunteer Interest Form submissions</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col card mb-4 pb-4">
+              <div class="card-header bg-primary text-white text-center">
+                Sponsors
+              </div>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item"><a href="admin_sponsorFormLeads.php" class="list-group-item list-group-item-action">View View Sponsorship Leads</a></li>
+              </ul>
+            </div>
+
+            <div class="col card mb-4 pb-4">
+              <div class="card-header bg-primary text-white text-center">
+                Members
+              </div>
+              <ul class="list-group list-group-flush mb-0">
+                <li class="list-group-item"><a href="admin_memberFormLeads.php" class="list-group-item list-group-item-action">View View Membership Applicants</a></li>
+              </ul>
+            </div>
+          </div>
           </div>
 
     </div>
