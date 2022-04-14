a301aacf6f4895373a9b01aa9afe1ec510f4d9c8
Darin2 <49596720+Darin2@users.noreply.github.com>
2022-04-14 09:01:21 -0500

    styling control panel

--- admin_controlpanel.php
+++ admin_controlpanel.php
@@ -15,24 +15,25 @@ header("Location: admin_loginpage.php");
 <main class="bg-dark">
   <body class="bg-dark">
     <!-- control panel container -->
-    <div class="container bg-light bg-gradient">
+    <div class="container bg-light bg-gradient mx-auto col-lg-4 col-md-6 col-sm-12 rounded">
 
-        <h1>Admin Control Panel</h1>
           <!-- admin links will go in this div -->
-          <div id="adminLinksContainer">
-            <div>
-              <p><a href="admin_contactFormLeads.php">View Contact Form submissions</a></p>
-              <p><a href="admin_volunteerInterestFormLeads.php">View Volunteer Interest Form submissions</a></p>
-              <p><a href="admin_registeredUsersList.php">View registered users</a></p>
-              <p><a href="admin_editFaqPage.php">Edit the FAQ Page</a></p>
-            </div>
-          </div>
+          <div class="mx-auto col-lg-6 col-md-8 col-sm-12 my-3 px-0 pt-5 bg-light bg-gradient rounded">
 
-          <div class="list-group">
-            <a href="admin_contactFormLeads.php" class="list-group-item list-group-item-action">View Contact Form submissions</a>
-            <a href="admin_volunteerInterestFormLeads.php" class="list-group-item list-group-item-action">View Volunteer Interest Form submissions</a>
-            <a href="admin_registeredUsersList.php" class="list-group-item list-group-item-action">View Registered Users</a>
-            <a href="admin_editFaqPage.php" class="list-group-item list-group-item-action">Edit the FAQ Page</a>
+            <h1 class="text-center display-4 py-1">Admin Controls</h1>
+
+            <!-- card attempt -->
+            <div class="card" style="width: 18rem;">
+              <div class="card-header bg-primary text-white text-center">
+                My Leads
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"><a href="admin_contactFormLeads.php" class="list-group-item list-group-item-action">View Contact Form submissions</a></li>
+                <li class="list-group-item"><a href="admin_volunteerInterestFormLeads.php" class="list-group-item list-group-item-action">View Volunteer Interest Form submissions</a></li>
+                <li class="list-group-item"><a href="admin_registeredUsersList.php" class="list-group-item list-group-item-action">View Registered Users</a></li>
+              </ul>
+            </div>
+
           </div>
 
     </div>
