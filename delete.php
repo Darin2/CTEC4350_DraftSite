72af9d99c37a24c763af9e89a33cd9ff7d0e5d96
vanessatorq <vmt8625@mavs.uta.edu>
2022-04-15 12:12:37 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,11 +4,13 @@
 //$conn = mysqli_connect("localhost", "vmt8625_class4350", "theGULF2022", "vmt8625_4350");
 // stored shared content
 include("dbconn.inc.php");
+include("shared.php");
+//echo "$component_HTMLHeader";
 
 // make database connection
-  //$conn = new PDO("mysql:host=localhost:3306;dbname=vmt8625_4350", "root", "root");
+ $conn = new PDO("mysql:host=localhost;dbname=vmt8625_4350", "vmt8625_class4350", "theGULF2022");
 //$conn = mysqli_connect("localhost", "vmt8625_class4350", "theGULF2022", "vmt8625_4350");
-$conn = dbConnect();
+//$conn = dbConnect();
 
 	// check if FAQ existed
 	$sql = "SELECT * FROM faqs WHERE id = ?";
