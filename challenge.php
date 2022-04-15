69266fd2a7e96afab2137a5b7064328cac5e3569
vanessatorq <vmt8625@mavs.uta.edu>
2022-04-15 12:27:13 -0500

    Update challenge.php

--- challenge.php
+++ challenge.php
@@ -4,7 +4,7 @@ echo "$component_HTMLHeader";?>
 <!doctype html>
 <html>
 <head>
-<title>Monthly Challenges | The G.U.L.F. </title>
+<title>Entrepreuner Challenges | The G.U.L.F. </title>
 <style>
 #menu {width: 100px; height: 300px; display: inline;}
 </style>
@@ -72,28 +72,28 @@ echo "$component_HTMLHeader";?>
 </div>
 
 <div id="m2" class="month">
-<h4 class="display-4 text-white text-center">February Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+<h4 class="display-4 text-white text-center">3 in 5 Challenge</h4>
+<p class="lead text-white text-center">Come up with three business ideas in five minutes or less, then write a business plan for your favorite one. <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 
 <div id="m3" class="month">
-<h4 class="display-4 text-white text-center">March Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+<h4 class="display-4 text-white text-center">Invention Challenge</h4>
+<p class="lead text-white text-center">Invent your own product and come up with a five minute business pitch as if you were pitching your product to real investors.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 	
 <div id="m4" class="month">
-<h4 class="display-4 text-white text-center">April Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+<h4 class="display-4 text-white text-center">Flipping Challenge</h4>
+<p class="lead text-white text-center">Find an item you can buy cheaply from a garage sale, thrift store or friend. Buy the item, and see how quickly you can sell it for a profit.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 	
 <div id="m5" class="month">
-<h4 class="display-4 text-white text-center">May Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+<h4 class="display-4 text-white text-center">Mentoring Challenge</h4>
+<p class="lead text-white text-center">Connect with a local business owner, either in person or virtually, and ask them what they've learned since starting their own business.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 	
 <div id="m6" class="month">
-<h4 class="display-4 text-white text-center">June Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+<h4 class="display-4 text-white text-center">Innovation Challenge</h4>
+<p class="lead text-white text-center">Think of your absolute favorite product. What do you like about it? What makes it great? Now, think of three ways this product could be even better.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 	
 <div id="m7" class="month">
