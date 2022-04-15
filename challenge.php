5185015d6d58ed3d5360d107f48f8b08201d3e97
vanessatorq <vmt8625@mavs.uta.edu>
2022-04-15 16:37:47 -0500

    making changes

--- challenge.php
+++ challenge.php
@@ -9,48 +9,12 @@ echo "$component_HTMLHeader";?>
 #menu {width: 100px; height: 300px; display: inline;}
 </style>
 
-<script>
-	function init(){
-        anchorArr = document.querySelectorAll("#menu a");
-    
-        for (let i=0; i < anchorArr.length; i++ ) {
-            anchorArr[i].onclick = function(){show(i+1)};
-        }
-	}
-    
-	function show(mNum){
-		// first, hide all months
-        mArr = document.querySelectorAll(".month");
-        for (i=0; i<mArr.length; i++){
-            mArr[i].style.display = "none";
-        }
-		// show the particular month
-		objId = "m"+mNum;
-		obj = document.getElementById(objId);
-		obj.style.display = "block";
-	}
-	
-   window.onload = init;
-</script>
 </head>
 
 <body>
-<main>
+<main>	
 	<?php echo "$component_Nav";?>
-   <div id="menu">
-	<a href="#" id="linkM1">January</a><br>
-	<a href="#" id="linkM2">February</a><br>
-	<a href="#" id="linkM3">March</a><br>
-	<a href="#" id="linkM4">April</a><br>
-	<a href="#" id="linkM5">May</a><br>
-	<a href="#" id="linkM6">June</a><br>
-	<a href="#" id="linkM7">July</a><br>
-	<a href="#" id="linkM8">August</a><br>
-	<a href="#" id="linkM9">September</a><br>
-	<a href="#" id="linkM10">October</a><br>
-	<a href="#" id="linkM11">November</a><br>
-	<a href="#" id="linkM12">December</a><br
-</div>
+	   
 <div id="container">
 </div>
 	
@@ -61,13 +25,23 @@ echo "$component_HTMLHeader";?>
     <div id="about-ChallengesRow" class="row d-flex align-items-center gulfBlueBG bg-gradient">
 		
       <section class="col-sm py-0">
-		     <h1 class="display-4 text-black text-center">Monthly Entrepreneur Challenge</h1>
-<p class="lead text-black text-center">Challenge your entrepreneurial skills each month with our challenges. Click on the current month to discover a new challenge!</p>
+		<h1 class="display-4 text-black text-center">Monthly Entrepreneur Challenge</h1>
+<p class="lead text-black text-center">Challenge your entrepreneurial skills with our challenges. Check back each month to discover a new challenge! Don't forget to show us how you do, tag us in your social media posts using the hashtag #TheGULF</p>
 		</section>
-		<section class="col-sm py-0">
-    <div id="m1" class="month">
-	
-<h4 class="display-4 text-white text-center">January Challenge</h4>
+<p id="demo"></p>
+
+<!-- script below to figure out the current month-->
+<script>
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let name = month[d.getMonth()];
+document.getElementById("demo").innerHTML = name;
+</script>
+		
+<section class="col-sm py-0">
+ <div id="m1" class="month">
+<h4 class="display-4 text-white text-center">Trade-up Challenge</h4>
 <p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 
@@ -96,34 +70,7 @@ echo "$component_HTMLHeader";?>
 <p class="lead text-white text-center">Think of your absolute favorite product. What do you like about it? What makes it great? Now, think of three ways this product could be even better.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
 </div>
 	
-<div id="m7" class="month">
-<h4 class="display-4 text-white text-center">July Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-	
-<div id="m8" class="month">
-<h4 class="display-4 text-white text-center">August Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-	
-<div id="m9" class="month">
-<h4 class="display-4 text-white text-center">September Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-	
-<div id="m10" class="month">
-<h4 class="display-4 text-white text-center">October Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-	
-<div id="m11" class="month">
-<h4 class="display-4 text-white text-center">November Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-	
-<div id="m12" class="month">
-<h4 class="display-4 text-white text-center">Decemeber Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+
 </div>
       </section>
     </div>
