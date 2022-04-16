c5b8ef72c591bb5975078045d109d0497faf1b7b
Darin <darinwhardin@gmail.com>
2022-04-16 11:17:34 -0500

    adding comments for clarity

--- challenge.php
+++ challenge.php
@@ -9,74 +9,82 @@ echo "$component_HTMLHeader";?>
 #menu {width: 100px; height: 300px; display: inline;}
 </style>
 
+<!-- script below to figure out the current month-->
+<script>
+	const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+	const d = new Date();
+	let name = month[d.getMonth()];
+	document.getElementById("demo").innerHTML = name;
+</script>
+
 </head>
 
 <body>
 <main>
-	<?php echo "$component_Nav";?>
+<?php echo "$component_Nav";?>
 
-<div id="container">
-</div>
-
+<!-- begin bootstrap container -->
 <div class="container-fluid px-0 mx-auto">
-  <div id="challengeJumbotron" class="jumbotron row ml-0">
 
-    <!-- challenges row -->
-    <div id="about-ChallengesRow" class="row d-flex align-items-center gulfBlueBG bg-gradient">
+	<!-- begin jumbotron -->
+	<div id="challengeJumbotron" class="jumbotron row ml-0">
 
-      <section class="col-sm py-0">
-		<h1 class="display-4 text-black text-center">Monthly Entrepreneur Challenge</h1>
-<p class="lead text-black text-center">Challenge your entrepreneurial skills with our challenges. Check back each month to discover a new challenge! Don't forget to show us how you do, tag us in your social media posts using the hashtag #TheGULF</p>
+		<!-- begin challenges row -->
+		<div id="about-ChallengesRow" class="row d-flex align-items-center gulfBlueBG bg-gradient">
+
+			<!-- begin title section -->
+			<section class="col-sm py-0">
+				<h1 class="display-4 text-black text-center">Monthly Entrepreneur Challenge</h1>
+				<p class="lead text-black text-center">Challenge your entrepreneurial skills with our challenges. Check back each month to discover a new challenge! Don't forget to show us how you do, tag us in your social media posts using the hashtag #TheGULF</p>
+			</section>
+			<!-- end title section -->
+
+			<p id="demo"></p>
+
+			<!-- begin months section -->
+			<section id="monthsSection" class="col-sm py-0">
+
+				<div id="m1" class="month">
+					<h4 class="display-4 text-white text-center">Trade-Up Challenge</h4>
+					<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+				<div id="m2" class="month">
+					<h4 class="display-4 text-white text-center">3 in 5 Challenge</h4>
+					<p class="lead text-white text-center">Come up with three business ideas in five minutes or less, then write a business plan for your favorite one. <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+				<div id="m3" class="month">
+					<h4 class="display-4 text-white text-center">Invention Challenge</h4>
+					<p class="lead text-white text-center">Invent your own product and come up with a five minute business pitch as if you were pitching your product to real investors.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+				<div id="m4" class="month">
+					<h4 class="display-4 text-white text-center">Flipping Challenge</h4>
+					<p class="lead text-white text-center">Find an item you can buy cheaply from a garage sale, thrift store or friend. Buy the item, and see how quickly you can sell it for a profit.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+				<div id="m5" class="month">
+					<h4 class="display-4 text-white text-center">Mentoring Challenge</h4>
+					<p class="lead text-white text-center">Connect with a local business owner, either in person or virtually, and ask them what they've learned since starting their own business.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+				<div id="m6" class="month">
+					<h4 class="display-4 text-white text-center">Innovation Challenge</h4>
+					<p class="lead text-white text-center">Think of your absolute favorite product. What do you like about it? What makes it great? Now, think of three ways this product could be even better.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
+				</div>
+
+			</div>
+			<!-- end challenges row -->
+
 		</section>
-<p id="demo"></p>
+		<!-- end months section -->
 
-<!-- script below to figure out the current month-->
-<script>
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-const d = new Date();
-let name = month[d.getMonth()];
-document.getElementById("demo").innerHTML = name;
-</script>
-
-<section class="col-sm py-0">
- <div id="m1" class="month">
-<h4 class="display-4 text-white text-center">Trade-up Challenge</h4>
-<p class="lead text-white text-center">This week, your challenge is to turn $5 into at least $25. <br> You get to keep every dollar you make, so get creative! <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-<div id="m2" class="month">
-<h4 class="display-4 text-white text-center">3 in 5 Challenge</h4>
-<p class="lead text-white text-center">Come up with three business ideas in five minutes or less, then write a business plan for your favorite one. <br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-<div id="m3" class="month">
-<h4 class="display-4 text-white text-center">Invention Challenge</h4>
-<p class="lead text-white text-center">Invent your own product and come up with a five minute business pitch as if you were pitching your product to real investors.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-<div id="m4" class="month">
-<h4 class="display-4 text-white text-center">Flipping Challenge</h4>
-<p class="lead text-white text-center">Find an item you can buy cheaply from a garage sale, thrift store or friend. Buy the item, and see how quickly you can sell it for a profit.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-<div id="m5" class="month">
-<h4 class="display-4 text-white text-center">Mentoring Challenge</h4>
-<p class="lead text-white text-center">Connect with a local business owner, either in person or virtually, and ask them what they've learned since starting their own business.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-<div id="m6" class="month">
-<h4 class="display-4 text-white text-center">Innovation Challenge</h4>
-<p class="lead text-white text-center">Think of your absolute favorite product. What do you like about it? What makes it great? Now, think of three ways this product could be even better.<br> Share your success with us on Instagram and use the hashtag #TheGulf <br> Check back next month for another challenge!</p>
-</div>
-
-
-</div>
-      </section>
-    </div>
-		</div>
 	</div>
-
+	<!-- end jumbotron -->
+</div>
+<!--end bootstrap container -->
 </main>
 <?php echo "$component_Footer";?>
 </body>
