e8c0293f2f10dc13838438abc0fe4f57e9197e48
NyricEvans <71023938+NyricEvans@users.noreply.github.com>
2022-04-19 12:42:48 -0500

    Create dbconn3.inc.php

---
+++ dbconn3.inc.php
@@ -0,0 +1,21 @@
+<?php
+############################################################################
+# Please 
+# (1) change the file name from dbconn.inc.example.php to dbconn.inc.php.
+# (2) edit the code below to provide your database user name, password, and database name
+#       -- NOTE: Add a new database user first in cPanel. (You only need to add one for all assignments and projects in this class.) 
+############################################################################
+
+function dbConnect(){
+	$host = "localhost"; // for uta.cloud server, "localhost" is the host name.  Do not edit.
+	$user = "nre6347_4350"; // put your own user name here.
+	$pwd = "theGULF2022"; // put your own database password here
+	$database = "nre6347_4350"; // put your database name here
+	$port = "3306"; // server-specific.  For uta.cloud, the port number is 3306 (the default port)
+ 
+	// initiate a new mysqli object to connect to the Database.  Store the mysqli object in a variable $conn.
+	$conn = new mysqli($host, $user, $pwd, $database, $port) or die("could not connect to server");
+	
+	// return $conn to the function call
+	return $conn;}
+?>
