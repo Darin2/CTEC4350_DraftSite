befb94460be1c677f6538f1f22ac5f0760aabc3a
Darin <darinwhardin@gmail.com>
2022-04-19 20:55:26 -0500

    logout page looks a bit nicer

--- admin_logoutpage.php
+++ admin_logoutpage.php
@@ -8,11 +8,15 @@ $_SESSION = array();
 include("shared.php");
 echo "$component_HTMLHeader";
 ?>
-  <body>
-    <main>
-      <h1> You've been logged out.</h2>
-    </main>
+<body class="bg-dark">
+<?php echo $component_Nav?>
+  <main class="bg-dark">
+  <div class="container bg-dark my-5">
+    <h1 class="text-white text-center display-4 py-3"> You have been logged out.</h2>
+    <p class='lead text-white text-center mx-auto'>To log in again, click <a href="admin_loginpage.php">here</a>.</p>
 
+  </div>
+    </main>
   <?php echo $component_Footer?>
   </body>
 </html>
