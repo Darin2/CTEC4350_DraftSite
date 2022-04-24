75e38e6f289d5d1dc87abdcfc8b3e7e07a7f5cfc
Darin <darinwhardin@gmail.com>
2022-04-23 22:05:58 -0500

    Revert "hiding the bridge pic hero section for now"
    This reverts commit 789be0c086aa38d90aa94ea1ba63b5ae0d04c8ca.

--- index.php
+++ index.php
@@ -16,20 +16,14 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
     <!--
 
     full width video. hidden on small screens.  not using it right now.
-
+    
     <div id="videoContainer" class="video-container d-none d-md-block">
       <iframe src="https://www.youtube.com/embed/p3XeN3K_3IQ?controls=0&amp;start=10&playlist=p3XeN3K_3IQ&loop=1&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div id="text">
         <h1 id="videoH1" class="display-1 text-center">Leadership & Focus For DFW Youth</h1>
       </div>
     </div>
-
      -->
-
-     <!--
-
-     huge bridge pic hero section. hidden on big screens.  not using it right now.
-
     <div id="homepageHeroJumbotron" class="d-lg-none d-md-none jumbotron row mx-4">
       <h1 id="bridgePicH1" class="display-1 text-white text-center">Leadership & Focus For DFW Youth</h1>
       <p class="lead text-white my-0 py-0">Delivering fun, positive growth opportunities to DFW's youth since 2016.</p>
@@ -37,8 +31,6 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
       </p>
     </div>
-
-    -->
 
   <!-- end innerContainer-->
 </div>
