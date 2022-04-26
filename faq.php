5467e1d4f559104954799ce2bed70b1071215391
vanessatorq <vmt8625@mavs.uta.edu>
2022-04-26 14:46:37 -0500

    faq
    styling FAQ page

--- faq.php
+++ faq.php
@@ -31,8 +31,11 @@ echo "$component_HTMLHeader";
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
 
+	<div id="faqBorder" class="col card text-center">
 
- <h2 class="display-1 text-white text-center">Frequently Asked Questions</h2>
+ <h2 class="display-1 text-black text-center">Frequently Asked Questions</h2>
+
+ <p class="lead">To give you more information about The G.U.L.F and what we offer to our community, here are some commonly asked questions. </p>
 
 <!-- show all FAQs in a panel -->
 <div class="container" style="margin-top: 50px; margin-bottom: 50px;">
@@ -65,12 +68,9 @@ echo "$component_HTMLHeader";
 		</div>
 	</div>
 </div>
-   <p class="lead">We've put together some commonly asked questions to give you more information about The G.U.L.F and what we offer to our community. <br> If you have a question that you can't find the answer to, please use the <a class="btn btn-primary btn-lg" href="contact.php" role="button">contact form</a> page.
+   <p class="lead">If you have a question that you can't find the answer to, please use the <a href="contact.php">contact form</a> and a member of our team will be in touch.
    </p>
-
-
- <p class="lead"> --OR-- If you have any questions that we haven't been able to answer we would love to hear from you!<br>Please complete the form found <a class="btn btn-primary btn-lg" href="contact.php" role="button">here</a> and a member of our team will be in touch.
-   </p>
+</div>
 
 <?php echo "$component_Footer";?>
 
