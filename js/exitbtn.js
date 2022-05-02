971fa8e2c9d4f25451de90d93a32a930d7f17735
pmyon <44069084+pmyon@users.noreply.github.com>
2022-05-02 14:50:00 -0500

    added comments to script

--- js/exitbtn.js
+++ js/exitbtn.js
@@ -1,4 +1,6 @@
+// when window loads
 window.onload = () => {
+  //remove exit button, or id="close-error", on click
   document.getElementById('close-error').onclick = function() {
     this.remove()
     return false;
