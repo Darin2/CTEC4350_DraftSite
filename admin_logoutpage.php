5a9b0b186e4c81ca47af02a94e05982434d7ac27
Darin <darinwhardin@gmail.com>
2022-05-03 09:07:30 -0500

    orange hyperlink instead of blue

--- admin_logoutpage.php
+++ admin_logoutpage.php
@@ -12,8 +12,8 @@ echo "$component_HTMLHeader";
 <?php echo $component_Nav?>
   <main class="bg-dark">
   <div class="container bg-dark my-5">
-    <h1 class="text-white text-center display-4 py-3"> You have been logged out.</h2>
-    <p class='lead text-white text-center mx-auto'>To log in again, click <a href="admin_loginpage.php">here</a>.</p>
+    <h1 class="text-white text-center display-4 py-3">You have been logged out.</h2>
+    <p class='lead text-white text-center mx-auto'>To log in again, click <a class="gulfOrangeText" href="admin_loginpage.php">here</a>.</p>
 
   </div>
     </main>
