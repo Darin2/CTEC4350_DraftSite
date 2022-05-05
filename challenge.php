99f7221a31a8c319b2a44cb5292c3468fb6145f5
Darin <darinwhardin@gmail.com>
2022-05-05 11:23:26 -0500

    commenting Challenge.php code

--- challenge.php
+++ challenge.php
@@ -17,9 +17,10 @@ echo "$component_HTMLHeader";?>
 	  <div class="card-body gulfBlueBG">
 	    <h5 id="challengeTitle" class="display-6 card-title text-center text-white">challenge title</h5>
 	    <p id="challengeDescription" class="lead col-sm-6 text-white text-left mx-auto">challenge description with social media reminder goes here (IG: thegulf_tx)</p>
+			<p class="lead col-sm-6 text-white text-left mx-auto">Share your success with us on Instagram and use the hashtag #TheGulf.<br><br>Check back next month for another challenge!</p>
 	  </div>
 
-	  <div class="card-footer text-muted text-black">
+	  <div class="card-footer text-muted text-center text-black">
 	    	<p id="daysLeft" class="card-text">"next challenge will appear in ____ days" goes here</p>
 	  </div>
 
@@ -32,20 +33,29 @@ echo "$component_HTMLHeader";?>
 <?php echo "$component_Footer";?>
 <!-- javascript that makes the content change each month -->
 <script>
+
+	/**********************************************************/
+	/*****************Setting up a date object*****************/
+	/**********************************************************/
+
+	const d = new Date();
+	console.log('d.getMonth() = ' + d.getMonth());
+
+	/**********************************************************/
+	/*****************Creating our arrays**********************/
+	/**********************************************************/
+
 	//create an array of months
 	const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-	//create a new Date object when someone loads this page
-	const d = new Date();
 
 	// store the current month in a variable
 	let currentMonth = month[d.getMonth()];
 	console.log('currentMonth = ' + currentMonth);
 
-	console.log('d.getMonth() = ' + d.getMonth());
-
+	//array of challenge titles
 	let challengeTitleArray = ["Trade Up","3 in 5","Invention","Flipping","Mentoring","Innovation"];
 
+	//array of challenge descriptions
 	let challengeDescriptionArray = [
 
 		"This week, your challenge is to turn $5 into at least $25. You get to keep every dollar you make, so get creative! <br>",
@@ -60,12 +70,18 @@ echo "$component_HTMLHeader";?>
 
 		"Think of your absolute favorite product. What do you like about it? What makes it great?<br> Now, think of three ways this product could be even better.<br>"]
 
-	let shareMsg = "<br>Share your success with us on Instagram and use the hashtag #TheGulf.<br><br>Check back next month for another challenge!<br>";
+	//make an array containing the number of days in each month, in order from january to december
+	let lengthOfMonthArray = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+
+
+
+	/**********************************************************/
+	/****Creating variables for each piece of the challenge****/
+	/**********************************************************/
 
 	let challengeTitleText = "";
 	let challengeDescriptionText = "";
-
-	console.log('Debug: on line 134 my switch statement evaluated d.month to equal ' + d.getMonth());
 
 	// Set the title of the event based on the current month
 	challengeTitleText = challengeTitleArray[d.getMonth()];
@@ -74,9 +90,6 @@ echo "$component_HTMLHeader";?>
 	//Set the description of the event based on the current month
 	challengeDescriptionText = challengeDescriptionArray[d.getMonth()];
 	console.log('challengeDescription = ' + challengeDescriptionArray[d.getMonth()]);
-
-	//make an array containing the number of days in each month, in order from january to december
-	let lengthOfMonthArray = [31,28,31,30,31,30,31,31,30,31,30,31];
 
 	//Get the number for today's date, e.g. "18" for april 18th
 	let todaysDateNumber = String(new Date().getDate()).padStart(2, '0');
@@ -90,10 +103,17 @@ echo "$component_HTMLHeader";?>
 	let daysUntilCountdownResets = daysInCurrentMonth - todaysDateNumber;
 	console.log('daysUntilCountdownResets = ' + daysUntilCountdownResets);
 
+
+
+
+	/***************************************************/
+	/****Set innerHTML of each challenge component*****/
+	/**************************************************/
+
 	//Print the current month at the top of the challengesContainer.
 	document.getElementById("challengeMonth").innerHTML = "Monthly Challenge: " + currentMonth;
 	//Print the description of the challenge with the "share on social media" message attached
-	document.getElementById("challengeDescription").innerHTML = challengeDescriptionText + ' ' + shareMsg;
+	document.getElementById("challengeDescription").innerHTML = challengeDescriptionText + ' ';
 	//Print the title of the challenge.
 	document.getElementById("challengeTitle").innerHTML = '<u>The ' + challengeTitleText + ' Challenge</u>';
 	//Print the remaining days for this challenge
