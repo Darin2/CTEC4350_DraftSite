61f5e01effb89fdceb332623e069c10c2e004b92
Darin <darinwhardin@gmail.com>
2022-05-05 21:34:23 -0500

    editing home page

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <h1 class="display-2 text-white text-center">Launching DFW Youth Toward Success</h1>
 
         <img src="img/illustration_success.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
-        <p class="lead text-white">We're a Texas-based 501(c)(3) Organization offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
+        <p class="lead text-white">We're a Texas-based <a href="https://www.charitynavigator.org/ein/811313299">501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
 
         <img src="img/illustration_mentoring.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
         <p class="lead text-white">By offering a uniquely combined mentoring program that is fostered and managed by teens in exciting peer to peer settings, teens are encouraged to make successful transitions from high school no matter their surrounding circumstances.</p>
@@ -34,22 +34,22 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
          Whether they're going to college or entering the workforce, we set G.U.L.F. members up for success with skills such as:
         </p>
 
-        <div class="row">
+        <div class="row my-3">
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 align-items-center d-flex justify-content-center display-4">Project Planning</h3>
         <img src="img/skill_projectplanning.svg" class="w-50 py-3 mx-auto d-block"></img>
         </div>
 
-        <div class="row">
+        <div class="row my-3">
         <img src="img/skill_organization.svg" class="w-50 py-3 mx-auto d-block"></img>
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center display-4">Organization</h3>
         </div>
 
-        <div class="row">
+        <div class="row my-3">
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center display-4">Business Development</h3>
         <img src="img/skill_businessdevelopment.svg" class="w-50 py-3 mx-auto d-block"></img>
         </div>
 
-        <div class="row">
+        <div class="row my-3">
         <img src="img/skill_entrepreneur.svg" class="w-50 py-3 mx-auto d-block"></img>
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center display-4">Entrepreneurial Thinking</h3>
         </div>
