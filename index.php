cc814fb5f12e5f6c0ad51f91fa5a56badc130757
Darin <darinwhardin@gmail.com>
2022-05-05 22:12:11 -0500

    updating home page

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <h1 class="display-2 text-white text-center">Launching DFW Youth Toward Success</h1>
 
         <img src="img/illustration_success.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
-        <p class="lead text-white">We're a Texas-based <a class="gulfOrangeText" href="https://www.charitynavigator.org/ein/811313299">501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
+        <p class="lead text-white">We're a Texas-based <a class="gulfOrangeText" href="https://www.charitynavigator.org/ein/811313299"><u>501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</u></p>
 
         <img src="img/illustration_mentoring.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
         <p class="lead text-white">By offering a uniquely combined mentoring program that is fostered and managed by teens in exciting peer to peer settings, teens are encouraged to make successful transitions from high school no matter their surrounding circumstances.</p>
@@ -54,44 +54,45 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center display-4">Entrepreneurial Thinking</h3>
         </div>
       </section>
-
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 bg-white mx-0 px-5">
       <!-- How To Get Involved -->
-      <section class="col-lg-6 col-md-8 col-sm-12 mt-0 mx-auto py-5 gulfBlueBG">
-        <h2 class="display-2 text-white text-center">Be A Part Of The Action</h2>
+      <section class="col-lg-10 col-md-10 col-sm-12 mt-0 mx-auto py-5 bg-white">
+        <h2 class="display-2 gulfBlueText text-center">Be A Part Of The Action</h2>
 
-        <div class="row">
+        <div class="row mx-auto d-flex justify-content-center">
 
-          <div class="card col-lg-4 col-md-4 col-sm-12">
+          <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
             <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Join</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body gulfBlueBG">
+              <h5 class="card-title text-white">Membership</h5>
+              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
 
-          <div class="card col-lg-4 col-md-4 col-sm-12">
+          <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
             <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Volunteer</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body gulfBlueBG">
+              <h5 class="card-title text-white">Volunteering</h5>
+              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
 
-          <div class="card col-lg-4 col-md-4 col-sm-12">
+          <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
             <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Sponsorship</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body gulfBlueBG">
+              <h5 class="card-title text-white">Sponsorship</h5>
+              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
 
         </div>
       </section>
 </div>
-<!-- end Bootstrap container -->
+
 
 </main>
 <div id="homepageGoogleMapsEmbed">
