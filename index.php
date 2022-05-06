cdaf4aaae845354db41d9aeab9a8aaae89faab17
Darin <darinwhardin@gmail.com>
2022-05-05 20:33:57 -0500

    expanding home page

--- index.php
+++ index.php
@@ -18,13 +18,14 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
   </video>
 </div>
 <!-- begin bootstrap container -->
-<div class="container gulfBlueBG">
+<div class="col-lg-12 col-md-12 col-sm-12 gulfBlueBG mx-0 px-0">
 	 <section class="col-lg-6 col-md-8 col-sm-12 mt-0 mx-auto py-5 gulfBlueBG">
         <h1 class="display-4 text-white">Launching DFW Youth Toward Success</h1>
         <img src="img/illustration_success.svg" class="w-50 py-3 mx-auto d-block"></img>
         <p class="lead text-white">We're a Texas-based 501(c)(3) Organization offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
         <img src="img/illustration_mentoring.svg" class="w-50 py-3 mx-auto d-block"></img>
         <p class="lead text-white">By offering a uniquely combined mentoring program that is fostered and managed by teens in exciting peer to peer settings, teens are encouraged to make successful transitions from high school no matter their surrounding circumstances.</p>
+        <img src="img/illustration_inspiration.svg" class="w-50 py-3 mx-auto d-block"></img>
         <p class="lead text-white">
          Whether they are going to college or preparing for life as young adults without college, they are encouraged and inspired by inquiring project planning, organization, business development, volunteering, and career and entrepreneurial skills. Thus giving them a fighting chance on living life beyond mediocrity.
         </p>
