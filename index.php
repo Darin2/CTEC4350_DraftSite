f2972b7a3bef1d2e923ec9a7bd3ae87e03d10beb
Darin <darinwhardin@gmail.com>
2022-05-05 22:58:50 -0500

    editing home page content

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <h1 class="display-2 text-white text-center">Launching DFW Youth Toward Success</h1>
 
         <img src="img/illustration_success.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
-        <p class="lead text-white">We're a Texas-based <a class="gulfOrangeText" href="https://www.charitynavigator.org/ein/811313299"><u>501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</u></p>
+        <p class="lead text-white">We're a Texas-based <a class="gulfOrangeText" href="https://www.charitynavigator.org/ein/811313299"><u>501(c)(3) organization</a></u> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
 
         <img src="img/illustration_mentoring.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
         <p class="lead text-white">By offering a uniquely combined mentoring program that is fostered and managed by teens in exciting peer to peer settings, teens are encouraged to make successful transitions from high school no matter their surrounding circumstances.</p>
@@ -55,36 +55,36 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         </div>
       </section>
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 bg-white mx-0 px-5">
+<div class="col-lg-12 col-md-12 col-sm-12 bg-dark bg-gradient mx-0 px-5">
       <!-- How To Get Involved -->
-      <section class="col-lg-10 col-md-10 col-sm-12 mt-0 mx-auto py-5 bg-white">
-        <h2 class="display-2 gulfBlueText text-center">Be A Part Of The Action</h2>
+      <section class="col-lg-10 col-md-10 col-sm-12 mt-0 mx-auto py-5 bg-dark bg-gradient">
+        <h2 class="display-2 text-white text-center">Be A Part Of The Action</h2>
 
         <div class="row mx-auto d-flex justify-content-center">
 
           <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
-            <img class="card-img-top" src="..." alt="Card image cap">
+            <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
             <div class="card-body gulfBlueBG">
               <h5 class="card-title text-white">Membership</h5>
-              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text text-white">Build your future with valuable skills and relationships that last a lifetime.</p>
               <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
 
           <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
-            <img class="card-img-top" src="..." alt="Card image cap">
+            <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
             <div class="card-body gulfBlueBG">
               <h5 class="card-title text-white">Volunteering</h5>
-              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text text-white">Make positive, impactful contributions to the success of DFW's next generation.</p>
               <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
 
           <div class="card col-lg-3 col-md-4 col-sm-12 mx-2 my-2 gulfBlueBG">
-            <img class="card-img-top" src="..." alt="Card image cap">
+            <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
             <div class="card-body gulfBlueBG">
               <h5 class="card-title text-white">Sponsorship</h5>
-              <p class="card-text text-white">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text text-white">Help us accomplish our mission to launch DFW youth toward success.</p>
               <a href="#" class="btn btn-primary bg-white gulfBlueText">Learn more</a>
             </div>
           </div>
