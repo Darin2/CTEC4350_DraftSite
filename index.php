0cd8d62b8d7a42f60f246e259445380007ad7e14
Darin <darinwhardin@gmail.com>
2022-05-05 21:55:48 -0500

    adding Be A Part Of The Action section

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
   </video>
 </div>
 <!-- begin bootstrap container -->
-<div class="col-lg-12 col-md-12 col-sm-12 gulfBlueBG mx-0 px-5 bg-gradient">
-	 <section class="col-lg-6 col-md-8 col-sm-12 mt-0 mx-auto py-5 gulfBlueBG bg-gradient">
+<div class="col-lg-12 col-md-12 col-sm-12 gulfBlueBG mx-0 px-5">
+	 <section class="col-lg-6 col-md-8 col-sm-12 mt-0 mx-auto py-5 gulfBlueBG">
 
         <h1 class="display-2 text-white text-center">Launching DFW Youth Toward Success</h1>
 
         <img src="img/illustration_success.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
-        <p class="lead text-white">We're a Texas-based <a href="https://www.charitynavigator.org/ein/811313299">501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
+        <p class="lead text-white">We're a Texas-based <a class="gulfOrangeText" href="https://www.charitynavigator.org/ein/811313299">501(c)(3) organization</a> offering solutions and opportunities to engage underserved teens in staying on the path to their success.</p>
 
         <img src="img/illustration_mentoring.svg" class="w-50 my-2 py-3 mx-auto d-block"></img>
         <p class="lead text-white">By offering a uniquely combined mentoring program that is fostered and managed by teens in exciting peer to peer settings, teens are encouraged to make successful transitions from high school no matter their surrounding circumstances.</p>
@@ -52,6 +52,42 @@ Link to the wireframes for this project: https://darinhardin550576.invisionapp.c
         <div class="row my-3">
         <img src="img/skill_entrepreneur.svg" class="w-50 py-3 mx-auto d-block"></img>
         <h3 class="lead text-white col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center display-4">Entrepreneurial Thinking</h3>
+        </div>
+      </section>
+
+      <!-- How To Get Involved -->
+      <section class="col-lg-6 col-md-8 col-sm-12 mt-0 mx-auto py-5 gulfBlueBG">
+        <h2 class="display-2 text-white text-center">Be A Part Of The Action</h2>
+
+        <div class="row">
+
+          <div class="card col-lg-4 col-md-4 col-sm-12">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Join</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div class="card col-lg-4 col-md-4 col-sm-12">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Volunteer</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div class="card col-lg-4 col-md-4 col-sm-12">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Sponsorship</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
         </div>
       </section>
 </div>
